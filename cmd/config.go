579a9fd377b7760ace9e131212943ed838ae246a
Dan Herrera <sonarius@shew.io>
2021-01-18 23:47:42 -0500

    cmd/config: update usage docs after recent output change

--- cmd/config.go
+++ cmd/config.go
@@ -10,9 +10,26 @@ import (
 var configUsage = `Usage:
 	%s config {server_definition}
 
-The config subcommand prints out whatever config has been parsed from the server definition
-specified at the path given by the 'server_definition' argument. If no argument
-is given, then 'server_definition' defaults to 'server.yaml'.
+The config subcommand prints out whatever config has been parsed from the server
+definition specified at the path given by the 'server_definition' argument. If
+no argument is given, then 'server_definition' defaults to 'server.yaml'.
+
+The output of this subcommand will include a JSON-formatted struct with the
+install, runtime, and config files for a server as they will be used during
+operations such as 'run' and 'install'. The struct includes not just what was
+parsed directly from the server definition, but also any defaults not set for
+any server files and options.
+
+The rendered struct has the following form:
+
+{
+	"install": {},	// install time options including server type and version
+	"run": {},		// run time options including memory restrictions
+	"configs": {	// server files that will be rendered during configuration
+		"path.extension": {},	// these include various files like
+		...						// server.properties, eula.txt, or spigot.yml
+	}
+}
 
 This subcommand primarily exists to help with testing configuration parsing and
 verifying config values with some other JSON-parsing tooling.
@@ -20,7 +37,7 @@ verifying config values with some other JSON-parsing tooling.
 
 var configCmd = Cmd{
 	Name:    "config",
-	Summary: "Print the parsed server definition",
+	Summary: "Print a parsed server definition",
 	Usage:   configUsage,
 	Exec: func(args ...string) error {
 		if len(args) > 1 {
