aabfb3c1f02d2dfbb4d8cfec9705e5da708bbb5c
Dan Herrera <sonarius@shew.io>
2021-01-21 00:25:01 -0500

    integration: fix suites post config subcommand update

--- integration/suites/bedrock-config.suite/verify.sh
+++ integration/suites/bedrock-config.suite/verify.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 # verify.sh
 
-if [[ "$(cat server.config | jq -r .Install.Kind)" != "bedrock" ]]; then
+if [[ "$(cat server.config | jq -r .install.Kind)" != "bedrock" ]]; then
   echo "expected install kind to be bedrock" && exit 1
 fi
 
-if [[ "$(cat server.config | jq -r .Configs.vanilla)" != "null" ]]; then
-  echo "expected vanilla config to be null" && exit 1
+if [[ "$(cat server.config | jq -r '.configs."server.properties".ServerPort')" != "19132" ]]; then
+  echo "expected port to be 19132" && exit 1
 fi
 
-if [[ "$(cat server.config | jq -r .Configs.bedrock.gamemode)" != "creative" ]]; then
+if [[ "$(cat server.config | jq -r '.configs."server.properties".Gamemode')" != "creative" ]]; then
   echo "expected gamemode to be creative" && exit 1
 fi
 
-if [[ "$(cat server.config | jq -r '.Configs.bedrock."player-movement-distance-threshold"')" != "0.9" ]]; then
+if [[ "$(cat server.config | jq -r '.configs."server.properties".PlayerMovementDistanceThreshold')" != "0.9" ]]; then
   echo "expected player movement distance threshold to be 0.9" && exit 1
 fi
